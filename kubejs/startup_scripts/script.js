299c0762e6fcd14477b8e1375a6eb539cbb58715
dctime <daniel.chu916@gmail.com>
2023-07-05 15:41:25 +0800

    Feature: Added a lot of mods

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -11,3 +11,15 @@ StartupEvents.registry('block', event => {
 	// Register new blocks here
 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
 })
+
+// Startup script
+StartupEvents.registry('fluid', event => {
+	// These first examples are 1.16.5 and 1.18.2 syntax
+	
+	// Basic "thick" (looks like lava) fluid with red tint
+	event.create('dusted_water')
+	  .thinTexture(0xc75600)
+	  .bucketColor(0xc75600)
+	  .displayName('Dusted Water')
+		.luminosity(1);
+  })
